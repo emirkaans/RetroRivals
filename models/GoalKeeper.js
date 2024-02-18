96594b47f81579a161711663e768c2413610e47d
Emir Kaan <emirkaansaricam@gmail.com>
2024-02-18 19:20:08 +0300

    goalkeeper metotlar güncellendi

--- models/GoalKeeper.js
+++ models/GoalKeeper.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { Player } from './Player.js';
+import { randomUpTo } from '../helpers.js';
 
 export class GoalKeeper extends Player {
   constructor(fullName, playerNumber, age, height, health, position, reflexes, bounce, oneOnOne) {
@@ -8,5 +9,17 @@ export class GoalKeeper extends Player {
     this.reflexes = reflexes;
     this.bounce = bounce;
     this.oneOnOne = oneOnOne;
+  }
+
+  isSaveCorner() {
+    return (this.reflexes + this.bounce) / 2 > randomUpTo(100);
+  }
+
+  getPenaltyScore() {
+    return (this.reflexes + this.bounce + this.oneOnOne) / 3;
+  }
+
+  getFreekickScore() {
+    return (this.reflexes + this.bounce) / 2;
   }
 }
